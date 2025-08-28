9aa07085efd0ec5b019631f88b5bf6461e018674
Andrew <andrew@192.168.2.4>
2025-08-29 02:59:06 +0700

    fix axios

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
+export const API_PRODUCTION_URL = import.meta.env.VITE_API_PRODUCTION_URL;
 export const ENDPOINTS = {
     status: '/status',
     auth: {
--- src/config/http.jsx
+++ src/config/http.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { API_BASE_URL } from './config.js';
-
+import { API_PRODUCTION_URL } from './config.js';
 const http = axios.create({
     baseURL: API_BASE_URL,
+    productionURL: API_PRODUCTION_URL,
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' },
 });
